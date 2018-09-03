9910a0b9074557970bf8fc53a7ceb599d8de6056
gabsfreitas <gabriel.freitas@lendico.com.br>
2018-09-03 00:37:57 -0300

    [REMOVE] Comentários desnecessários]

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ ACCESS_TOKEN = os.environ['FB_ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['FB_VERIFY_TOKEN']
 bot = Bot(ACCESS_TOKEN)
 
-#We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
 
 def receive_message():
@@ -31,15 +30,11 @@ def receive_message():
     return "Message Processed"
 
 def verify_fb_token(token_sent):
-    #take token sent by facebook and verify it matches the verify token you sent
-    #if they match, allow the request, else return an error 
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
-#uses PyMessenger to send response to user
 def send_message(recipient_id, response):
-    #sends user the text message provided via input response parameter
     bot.send_text_message(recipient_id, response)
     return "success"
 
