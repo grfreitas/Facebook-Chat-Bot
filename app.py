5bf04ca96ccd5c224f783f27b3dce923e682544b
Ubuntu <ubuntu@ip-172-31-43-151.us-east-2.compute.internal>
2018-09-05 05:08:35 +0000

    [REFACTOR] Split GET and POST route function into individual ones

--- app.py
+++ app.py
@@ -8,39 +8,30 @@ ACCESS_TOKEN = os.environ['FB_ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['FB_VERIFY_TOKEN']
 bot = Bot(ACCESS_TOKEN)
 
-@app.route("/", methods=['GET', 'POST'])
-
+@app.route("/", methods=['POST'])
 def receive_message():
-    
     try:
-
-        if request.method == 'GET':
-
-            token_sent = request.args.get("hub.verify_token")
-            return verify_fb_token(token_sent)
-
-        elif request.method == 'POST':
-            output = request.get_json()
-            for event in output['entry']:
-                for info in event['messaging']:
-                    if 'message' in info:
-                        recipient_id = info['sender']['id']
-                        response = fb_msg_handler(info)
-                        send_message(recipient_id, response)
-
-        return "Message Processed"
-
-    except Exception as e: 
+        output = request.get_json()
+        if 'messaging' in output['entry'][0]:
+            if 'message' in output['entry'][0]['messaging'][0]:
+                recipient_id = output['entry'][0]['messaging'][0]['sender']['id']
+                response = fb_msg_handler(output)
+                send_message(recipient_id, response)
+        return 'Message Processed.'
+    except Exception as e:
         print(e)
+        return 'Error.'
 
-def verify_fb_token(token_sent):
+@app.route("/", methods=['GET'])
+def verify_token():
+    token_sent = request.args.get('hub.verify_token')
     if token_sent == VERIFY_TOKEN:
-        return request.args.get("hub.challenge")
-    return 'Invalid verification token'
+        return request.args.get('hub.challenge')
+    return 'Invalid Verification Token'
 
 def send_message(recipient_id, response):
     bot.send_text_message(recipient_id, response)
-    return "success"
+    return 'Message Sent.'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- brain.py
+++ brain.py
@@ -1,3 +1,21 @@
+#-*- coding: utf-8 -*-
+
+from datetime import datetime
+import pytz
+
+tz = pytz.timezone('America/Sao_Paulo')
+
 def fb_msg_handler(info):
-    print(info)
-    return 'yo dawg'
+    try:
+        message_sent_at     = datetime.fromtimestamp(info['entry'][0]['messaging'][0]['timestamp']/1e3, tz=tz)
+        message_received_at = datetime.fromtimestamp(info['entry'][0]['time']/1e3, tz=tz)
+
+        message_text = info['entry'][0]['messaging'][0]['message']['text']
+
+        response = 'Mensagem: '+str(message_text)+'\nEnviada em: '+str(message_sent_at)+'\nRecebida em: '+str(message_received_at)
+
+    except Exception as e:
+        print(e)
+        response = 'Erro.'
+
+    return response
