b4b23383971af71553614230173f97affa3a7b4e
Ubuntu <ubuntu@ip-172-31-43-151.us-east-2.compute.internal>
2018-09-05 08:12:24 +0000

    [ADD] Weather report on cities and huge refactor on functions locations

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, request
-from pymessenger.bot import Bot
-from brain import fb_msg_handler
+from brain import msg_handler
 import os
 
 app = Flask(__name__)
 ACCESS_TOKEN = os.environ['FB_ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['FB_VERIFY_TOKEN']
-bot = Bot(ACCESS_TOKEN)
 
 @app.route("/", methods=['POST'])
 def receive_message():
@@ -14,9 +12,7 @@ def receive_message():
         output = request.get_json()
         if 'messaging' in output['entry'][0]:
             if 'message' in output['entry'][0]['messaging'][0]:
-                recipient_id = output['entry'][0]['messaging'][0]['sender']['id']
-                response = fb_msg_handler(output)
-                send_message(recipient_id, response)
+                msg_handler(output)
         return 'Message Processed.'
     except Exception as e:
         print(e)
@@ -28,10 +24,6 @@ def verify_token():
     if token_sent == VERIFY_TOKEN:
         return request.args.get('hub.challenge')
     return 'Invalid Verification Token'
-
-def send_message(recipient_id, response):
-    bot.send_text_message(recipient_id, response)
-    return 'Message Sent.'
 
 if __name__ == "__main__":
     app.run(debug=True)
--- brain.py
+++ brain.py
@@ -1,21 +1,67 @@
 #-*- coding: utf-8 -*-
 
+from requests import get
 from datetime import datetime
+from pymessenger.bot import Bot
+import os
 import pytz
 
+
+ACCESS_TOKEN = os.environ['FB_ACCESS_TOKEN']
+bot = Bot(ACCESS_TOKEN)
+
 tz = pytz.timezone('America/Sao_Paulo')
+climatempo_token = os.environ['CLIMATEMPO_TOKEN']
 
-def fb_msg_handler(info):
+def send_message(recipient_id, response):
+    bot.send_text_message(recipient_id, response)
+    return 'Message Sent.'
+
+def get_weather(location):
+    id_call = 'http://apiadvisor.climatempo.com.br/api/v1/locale/city?name={}&token={}'.format(location, climatempo_token)
+
+    if len(get(id_call).json()) > 0:
+        id = get(id_call).json()[0]['id']
+
+        weather_call = 'http://apiadvisor.climatempo.com.br/api/v1/weather/locale/{}/current?token={}'.format(id, climatempo_token)
+        weather = get(weather_call).json()
+
+        temperature = weather['data']['temperature']
+        condition   = weather['data']['condition']
+        humidity    = weather['data']['humidity']
+        location    = weather['name'] + '-' + weather['state']
+
+        return 'Em {} está fazendo {} graus Celsius, umidade relativa de {}% e com condição de {}'.format(location, temperature, humidity, condition.lower())
+    else:
+        return 'Cidade Inválida! Por favor, tente novamente :)'
+
+def msg_handler(info):
+    entry = info['entry'][0]
+    recipient_id = entry['messaging'][0]['sender']['id']
     try:
-        message_sent_at     = datetime.fromtimestamp(info['entry'][0]['messaging'][0]['timestamp']/1e3, tz=tz)
-        message_received_at = datetime.fromtimestamp(info['entry'][0]['time']/1e3, tz=tz)
+        msg_sent_at     = datetime.fromtimestamp(entry['messaging'][0]['timestamp']/1e3, tz=tz)
+        msg_received_at = datetime.fromtimestamp(entry['time']/1e3, tz=tz)
 
-        message_text = info['entry'][0]['messaging'][0]['message']['text']
+        msg_params = entry['messaging'][0]['message']
 
-        response = 'Mensagem: '+str(message_text)+'\nEnviada em: '+str(message_sent_at)+'\nRecebida em: '+str(message_received_at)
+        if 'text' in msg_params:
+            msg_text = msg_params['text'].lower()
+        else: msg_text = ''
+
+        if 'nlp' in msg_params:
+            if 'entities' in msg_params['nlp']:
+                msg_nlp_entities = msg_params['nlp']['entities']
+
+        if 'temperatura' in msg_text and 'location' in msg_nlp_entities:
+            location = msg_nlp_entities['location'][0]['value'].title()
+            send_message(recipient_id, 'Vou ver para você! ^_^')
+            response = get_weather(location)
+            send_message(recipient_id, response)
+
+        else: send_message(recipient_id, 'Mensagem: '+str(msg_text)+'\nEnviada em: '+str(msg_sent_at)+'\nRecebida em: '+str(msg_received_at))
 
     except Exception as e:
         print(e)
-        response = 'Erro.'
+        send_message(recipient_id, 'Erro. :poop:')
 
-    return response
+    return
